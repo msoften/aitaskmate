f70154709d697c03184047ac762d095d37a464bb
Moses Mwangi <moses.soft.eng@gmail.com>
2023-06-13 22:06:41 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -8,10 +8,13 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
+// root.render(
+// 	<React.StrictMode>
+// 	  <App />
+// 	</React.StrictMode>
+//   );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
